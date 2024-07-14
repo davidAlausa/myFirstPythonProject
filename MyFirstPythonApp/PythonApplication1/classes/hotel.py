5c01651c5f4980ec7742a39977a899082ba99a25
K00270915 <K00270915@student.tus.ie>
2024-07-14 19:08:08 +0100

    14/07/2024 - Added Monster Fight Functionality and more validation

--- MyFirstPythonApp/PythonApplication1/classes/hotel.py
+++ MyFirstPythonApp/PythonApplication1/classes/hotel.py
@@ -1,59 +1,13 @@
 import random
 import time
-import monster
 
 class Hotel:
     def __init__(this):
         this.floor = 1
-        this.monster = random.randint(2,5)
-        this.key = random.randint(2,5)
-        this.keyInventoryCount = 0
 
-    def keyFound(this):
-        print('\n\n\tYou found a Key! You now have ' + thiskeyInventoryCount + ' keys in your possession.')
-
-        if this.keyInventoryCount != 3:
-
-            print ('Good Luck!')
-            thiskeyInventoryCount += 1
-        
-        elif thiskeyInventoryCount == 3:
-
-            print ('This means that you can escape!!! Head back to floor one NOW!!!')
-
-    def monsterEvent():
-        CRED = '\033[91m'
-        CEND = '\033[0m'
-        print(CRED + '\n\n\tmuhhaahhahahHAHAHAHHAHAH' + CEND)
-        print("\n\n\tIt's the " + CRED + "MONSTER" + CEND +"\n\n\t\t! Enter 'A' to ATTACK or Enter 'D' to Defend")
-        print('\n\n THE ATTACK')
-        time.sleep(2)
-        print('\n\BEGINS')
-        time.sleep(2)
-        print('\n\tNOW\n\n----------------------------------------------------------------------------\n')
-
-        newMonster = Monster()
-    def emptyFloor():
-        print('still wip')
-
-    def checkElevatorChoice(this, choice):
-        print('\n\n\tGoing to floor' + choice + '. Opening door in:')
-        time.sleep(2)
-        print('\n\t3...')
-        time.sleep(2)
-        print('\n\t2....')
-        time.sleep(2)
-        print('\n\t1...')   
-
-        this.floor = choice
-
-        if choice == this.monster:
-            this.monsterEvent()
-        
-        elif choice == this.key:
-            this.keyFound()
-        
-        else:
-            this.emptyFloor()
-
+    def getFloor(this):
+        return this.floor
+    
+    def setFloor (this, newValue):
+        this.floor = newValue
         
--- MyFirstPythonApp/PythonApplication1/classes/monster.py
+++ MyFirstPythonApp/PythonApplication1/classes/monster.py
@@ -2,6 +2,17 @@ import random
 import time
 
 class Monster:
+    def __init__(this):
+        this.floorLevel = random.randint(2,5)
+        this.lives = 3
 
+    def moveMonster(this):
+        this.floorLevel = random.randint(2,5)
+
+    def getLives(this):
+        return this.lives
+
+    def setLives(this, newValue):
+        this.lives = newValue
 
         
